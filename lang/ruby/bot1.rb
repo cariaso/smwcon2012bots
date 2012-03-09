927b273a4e26e41dc7954f9c26b9c1ce005bc19a
Michael Cariaso <cariaso@gmail.com>
2012-03-09 11:40:49 -0500

    improved comments

--- lang/ruby/bot1.rb
+++ lang/ruby/bot1.rb
@@ -1,4 +1,5 @@
 # need to remove the 'private' from mediawiki-gateway/lib/media_wiki/gateway.rb
+# since the standard version makes make_api_request() inaccessible
 
 # git clone git://github.com/jpatokal/mediawiki-gateway.git
 $LOAD_PATH << 'mediawiki-gateway/lib'
@@ -7,7 +8,7 @@ require 'media_wiki'
 
 
 mw = MediaWiki::Gateway.new("http://www.semantic-mediawiki.org/w/api.php")
-@userquery = "
+userquery = "
 {{#ask: [[Category:City]] [[located in::Germany]] 
 | ?population 
 | ?Area
@@ -15,13 +16,13 @@ mw = MediaWiki::Gateway.new("http://www.semantic-mediawiki.org/w/api.php")
 "
 
 
-form_data = { 'action' => 'ask', 'query' => @userquery }
+form_data = { 'action' => 'ask', 'query' => userquery }
 xml, dummy = mw.make_api_request(form_data)
 results = xml.elements["query/results"]
 
 
 if results != nil
-  results.each do |p| # iterate over the results and grab the full text for each page
+  results.each do |p| 
     puts p.attributes["fulltext"]
   end
 end
