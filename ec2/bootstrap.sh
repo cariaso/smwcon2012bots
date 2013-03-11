d24fae83852e98d9289bcf931de51b3bd6e363ec
cariaso <cariaso@gmail.com>
2013-03-11 18:44:07 -0300

    Update bootstrap.sh

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-sudo yum -y install git gcc autoconf gmp gmp-devel gmp-static python-devel python-crypto gcc 
+sudo yum -y install git gcc autoconf gmp gmp-devel gmp-static python-devel python-crypto gcc python-setuptools subversion
 sudo easy_install pip
 sudo pip install argparse
 sudo pip install fabric
