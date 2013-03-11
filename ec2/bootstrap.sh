0ae7b60d83af08f9db563278a581c0e888d2837d
cariaso <cariaso@gmail.com>
2013-03-11 17:47:04 -0300

    Update bootstrap.sh

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -1,10 +1,8 @@
 #!/bin/env bash
 
-sudo yum -y install git 
+sudo yum -y install git gcc autoconf gmp gmp-devel gmp-static python-devel python-crypto gcc 
 sudo easy_install pip
 sudo pip install argparse
-
-#sudo yum -y install gcc autoconf gmp gmp-devel gmp-static python-devel python-crypto gcc
 sudo pip install fabric
 
 git clone git://github.com/cariaso/smwcon2012bots.git
