5c34169cd7f2e69ec20752adc9121ea1d3e47ef7
cariaso <cariaso@gmail.com>
2013-03-14 14:27:12 -0400

    alternative installler?

---
+++ ec2/force-install-smw.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+sudo yum -y install git gcc autoconf gmp gmp-devel gmp-static python-devel python-crypto gcc python-setuptools subversion
+sudo easy_install pip
+sudo pip install argparse
+sudo pip install fabric
+
+git clone git://github.com/cariaso/smwcon2012bots.git
+cd smwcon2012bots/ec2
+./install-semantic-mediawiki.py --yes $@
+
