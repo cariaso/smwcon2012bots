42e87f3645709df30b8b516f9fa7d77204b5c308
cariaso <cariaso@gmail.com>
2013-03-16 03:17:55 -0400

    make a decent summary for each site

--- lang/perl/check_gzip2.pl
+++ lang/perl/check_gzip2.pl
@@ -4,97 +4,63 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-
 use MediaWiki::API;
 # http://search.cpan.org/~exobuzz/MediaWiki-API-0.37/lib/MediaWiki/API.pm
-
-
-
-my @titles = ();
-
-sub remember_titles {
-    my ($ref) = @_;
-    foreach (@$ref) {
-	push @titles, $_->{title};
-    }
-}
-
 
 my $mw_api_url = 'http://semantic-mediawiki.org/w/api.php';
 
 my $mw = MediaWiki::API->new();
 $mw->{config}->{api_url} = $mw_api_url;
 
-my $ref = $mw->list ( { action => 'query',
-			list => 'categorymembers',
-			cmtitle => 'Category:Wiki_of_the_Month',
-			cmnamespace => 0,
-			cmlimit=>'100' },
-		      { max => 4, hook => \&remember_titles } )
-    || warn $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+my $response = $mw->api( {
+        action     => 'askargs',
+        conditions => 'Category:SMW_site',
+        printouts  => 'Homepage|APIURL',
+	parameters =>'|format=table',
 
+	# printouts don't work with these parameters above
+	#parameters =>'|sort=Modification date|order=desc',
+        })
+           || warn $mw->{error}->{code} . ': ' . $mw->{error}->{details};
 
-my $regex = qr/
-        (                   # start of bracket 1
-        {                   # match an opening curly
-            (?:
-                [^{}]++     # one or more non angle brackets, non backtracking
-                  |
-                (?1)        # recurse to bracket 1
-            )*
-        }                   # match a closing curly
-        )                   # end of bracket 1
-        /x;
+my $results = $response->{query}->{results};
+
 
 
 my $ua = LWP::UserAgent->new;
 my $can_accept = HTTP::Message::decodable;
-my $mw_checker = MediaWiki::API->new();
 
-foreach my $title (@titles) {
-    
-    my $page     = $mw->get_page( { title => $title } );
-    my $pagetext = $page->{'*'};
-    my @templates = $pagetext =~ m/$regex/g;
-    my @url = grep /^\s*\|\s*url\s*=/i, split(/\n/, ($templates[0]||''));
-    my ($address) = ($url[0]||'') =~ /(http[^\s\|\}]+)/;
-    #next unless $address;
-    #next unless $address =~ /index.php/;
+foreach my $name (sort keys %$results) {
+    next if $name =~ /^File:/;
+    my $homepage = $results->{$name}->{'printouts'}->{'Homepage'}[0] || '';
+    my $site_api_url = $results->{$name}->{'printouts'}->{'APIURL'}[0] || '';
 
 
-    #print Dumper \@infoboxes, \@url, $address;
-    #print "$title\t::\t$address\n";
 
+    # if no APIURL was provided, make a decent guess
+    if (!$site_api_url) {
+	$site_api_url = $homepage .'/api.php';
+	$site_api_url =~ s!//api!/api!;
+	$site_api_url =~ s!/wiki/.*api.php!/wiki/api.php!;
+	$site_api_url =~ s!/index.php.*!/api.php!;
+	
+    }
+
+
+
+    # Check for gzip compression on a json response
+    my $address = "$site_api_url?action=smwinfo&format=json";
     my $response = $ua->get($address, 
 			    'Accept-Encoding' => $can_accept);
-    my $encoding = $response->{_headers}->{'content-encoding'};
-    $encoding ||= '';
-    $address ||= '';
-    print "$encoding\t$title\t::\t$address\n";
-    next;
-    #print Dumper $response->{_headers};
-    #print $response->decoded_content;
-
-    my $api_url = $address;
-    $api_url =~ s/index.php.*/api.php/;
-
-    $mw_checker->{config}->{api_url} = $api_url;
-
-
-    eval {
-	my $ref = $mw_checker->api ( { 
-	    action => 'query',
-	    prop=>'info',
-	    titles=>'Main Page',
-				     },
-	    );
-	
-	if (exists($ref->{query})) {
-	    print Dumper $ref;
-	}
-    };
-    if ($@) {
-	print "Unable to work wth $title\n";
+    my $code = $response->{_rc};
+    my $text = $response->decoded_content;
+    my $ok = '';
+    if ($text =~ /^\{.*\}$/) {
+	$ok = 'OK';
     }
-}
+    #print $text;
+    #print Dumper $response;
+    my $encoding = $response->{_headers}->{'content-encoding'} || '';
 
+    print "$ok\t$code\t$encoding\t$name           \t$site_api_url\n";
+}
